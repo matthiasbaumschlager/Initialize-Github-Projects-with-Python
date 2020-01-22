50e3e93613ec3e475f18f59d7729fb8263f3bd2a
Matthias Baumschlager <matthias.baumschlager@gmail.com>
2020-01-22 14:29:57 +0100

    Add existing files

---
+++ config.py
@@ -0,0 +1,5 @@
+class Variables:
+	def __init__(self):
+		self.contribution_projects_folder=""
+		self.learning_projects_folder="/home/matthias/My Files/04 Growth/Learning/Coding/"
+		self.Languages =["LaTex","Python"]
