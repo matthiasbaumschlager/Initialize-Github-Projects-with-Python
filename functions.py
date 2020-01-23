cf10b2dadc608eb3a29af92eb9cf20c935ca8c18
Matthias Baumschlager <matthias.baumschlager@gmail.com>
2020-01-23 07:28:48 +0100

    Added _clone_repo_to_project_folder

--- functions.py
+++ functions.py
@@ -17,3 +17,9 @@ def _get_clone_url(repo_object):
 def _create_repo_with_all_properties(user,repo_name,repo_description,auto_init_bool,private_bool):
 	return user.create_repo(repo_name, description=repo_description, auto_init=auto_init_bool, private=private_bool)
 
+def _clone_repo_to_project_folder(repo_object, project_folder):
+	try:
+		os.mkdir(os.path.join(project_folder,repo_object.name))
+		return true
+	except:
+		print("The repository could not be cloned to your selected Project-Folder!")
