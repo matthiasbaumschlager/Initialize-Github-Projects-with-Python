e7c61534e612b7c3cdd980896947025a9572df02
Matthias Baumschlager <matthias.baumschlager@gmail.com>
2020-01-23 07:26:15 +0100

    Added existing code

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 class Variables:
 	def __init__(self):
-		self.contribution_projects_folder=""
+		self.contribution_projects_folder="/home/matthias/My Files/02 Contribution/Freelance/Coding/"
 		self.learning_projects_folder="/home/matthias/My Files/04 Growth/Learning/Coding/"
-		self.Languages =["LaTex","Python"]
+		self.Languages =["LaTex","Python", "Plain Text"]
+		# Github Login - Data
+		self.access_token="66b610dc8cdd435aa97041a2aa2921e9e3703326"
---
+++ script.py
@@ -0,0 +1,14 @@
+######## Imports ########
+import os
+from functions import 
+import config.Variables
+######## Imports ########
+
+
+
+
+
+
+
+if __name__ == "__main__":
+	c=Script()
