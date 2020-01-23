ec47f92878c2c960e58e3a0084baaf8657a53728
Matthias Baumschlager <matthias.baumschlager@gmail.com>
2020-01-23 08:48:24 +0100

    Finished backend (without testing)

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 class Variables:
-	def __init__(self):
-		self.contribution_projects_folder="/home/matthias/My Files/02 Contribution/Freelance/Coding/"
-		self.learning_projects_folder="/home/matthias/My Files/04 Growth/Learning/Coding/"
-		self.Languages =["LaTex","Python", "Plain Text"]
-		# Github Login - Data
-		self.access_token="66b610dc8cdd435aa97041a2aa2921e9e3703326"
+	contribution_projects_folder="/home/matthias/My Files/02 Contribution/Freelance/Coding/"
+	learning_projects_folder="/home/matthias/My Files/04 Growth/Learning/Coding/"
+	Languages =["LaTex","Python", "Plain Text"]
+	# Github Login - Data
+	access_token="66b610dc8cdd435aa97041a2aa2921e9e3703326"
--- script.py
+++ script.py
@@ -1,14 +1,30 @@
 ######## Imports ########
+#Packages
 import os
-from functions import 
-import config.Variables
+import time
+#Other files
+import functions as Functions
+from config import Variables as Config
 ######## Imports ########
 
+class Script:
+	def __init__(self):
 
 
 
 
+
+		# Backend - doing all the work after (correct) user input.
+		self.user = Functions._get_github_user(Functions._initiate_github_instance(Config.access_token))
+		repo = Functions._create_repo_with_all_properties(repo_name, repo_description, auto_init, private_repo)
+		if repo != False:
+			if(Functions._clone_repository_to_project_folder):
+				print("""Your new GitHub-Project was successfully created and is already cloned to your desired projec-folder!
+					Enjoy your work :)""")
+				time.sleep(4.2)
 
 
 if __name__ == "__main__":
 	c=Script()
+
+
